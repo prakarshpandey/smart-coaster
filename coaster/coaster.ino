7198865ef3ec71b6078632ef27d6de8e4bd0b43a
jacquigiese <jacqui.giese@gmail.com>
2017-10-27 22:02:51 -0700

    add varaible for temp input

--- coaster/coaster.ino
+++ coaster/coaster.ino
@@ -1,7 +1,10 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
+// define constants and variables
 #define SENSOR_PORT 2
+#define LED_PORT 13
+float tempInput
 
 OneWire oneWire(SENSOR_PORT);
 
@@ -11,11 +14,19 @@ void setup(void)
 {
   Serial.begin(9600);
   sensors.begin();
+  pinMode(LED_PORT, OUTPUT); 
 }
 
 void loop(void)
 {
+  digitalWrite(LED_PORT, LOW);
+  delay(1000);
+  digitalWrite(LED_PORT, HIGH);
+  
   sensors.requestTemperatures();
   Serial.println(sensors.getTempCByIndex(0));
+  tempInput = sensors.getTempCByIndex(0);
   delay(1000);
 }
+
+
