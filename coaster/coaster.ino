8efb3436f069d74dd0cedf44065b92ee6fb675da
CS70 Flamingo <jgiese@cs.hmc.edu>
2017-10-27 21:29:14 -0700

    Temp data

---
+++ coaster/coaster.ino
@@ -0,0 +1,21 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define SENSOR_PORT 2
+
+OneWire oneWire(SENSOR_PORT);
+
+DallasTemperature sensors(&oneWire);
+
+void setup(void)
+{
+  Serial.begin(9600);
+  sensors.begin();
+}
+
+void loop(void)
+{
+  sensors.requestTemperatures();
+  Serial.print(sensors.getTempCByIndex(0));
+  delay(1000);
+}
